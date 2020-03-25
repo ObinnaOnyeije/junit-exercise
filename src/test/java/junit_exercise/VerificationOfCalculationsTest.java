33e4d26d04026b64efdcc41f9e72c2236083787a
ObinnaOnyeije <obinnaonyeije@yahoo.com>
2020-03-25 00:03:40 -0400

    Updated tests

--- src/test/java/junit_exercise/VerificationOfCalculationsTest.java
+++ src/test/java/junit_exercise/VerificationOfCalculationsTest.java
@@ -10,21 +10,42 @@ class VerificationOfCalculationsTest {
 	double result;
 	double a = 2;
 	double b = 2;
+	double[] operands = {2, 2.5, 3.25, Double.MAX_VALUE, Double.MIN_VALUE};
 	String[] operators = {"+", "-", "*", "/"};
-	double[] answers = {4, 0, 4, 1};
+	double[] answerSet1 = {4, 0, 4, 1};
+	double[] answerSet2 = {5.75, -0.75, 8.125, (2.5/3.25)};
 
 	@Test
 	void testMain() {
 		for (int i = 0; i < operators.length; i++) {
-			String[] args = {Double.toString(a), operators[i], Double.toString(b)};
+			String[] args = {Double.toString(operands[0]), operators[i], Double.toString(operands[0])};
 			Calculator.main(args);
-			assertEquals(answers[i], Calculator.getResult());
+			assertEquals(answerSet1[i], Calculator.getResult());
 		}
+		
+		for (int i = 0; i < operators.length; i++) {
+			String[] args = {Double.toString(operands[1]), operators[i], Double.toString(operands[2])};
+			Calculator.main(args);
+			assertEquals(answerSet2[i], Calculator.getResult());
+		}
+		
+		String[] overflowArgs1 = {Double.toString(operands[3]), operators[0], Double.toString(operands[3])};
+		Calculator.main(overflowArgs1);
+		assertEquals(Double.POSITIVE_INFINITY, Calculator.getResult());
+		
+		String[] overflowArgs2 = {Double.toString(operands[4]), operators[1], Double.toString(operands[4])};
+		Calculator.main(overflowArgs2);
+		assertEquals(0, Calculator.getResult());
+		
+		String[] overflowArgs3 = {Double.toString(operands[3]), operators[2], Double.toString(operands[3])};
+		Calculator.main(overflowArgs3);
+		assertEquals(Double.POSITIVE_INFINITY, Calculator.getResult());
+		
 		String[] noArgs = {};
 		Calculator.main(noArgs);
 		assertEquals(Double.MIN_VALUE, Calculator.getResult());
 		
-		String[] falseOp = {Double.toString(a), "b", Double.toString(b)};
+		String[] falseOp = {Double.toString(operands[0]), "b", Double.toString(operands[0])};
 		Calculator.main(falseOp);
 		assertEquals(Double.MIN_VALUE, Calculator.getResult());
 		
